4d446b2b333ba589e26acc920f60fb53c235a889
shenyang <shenyang@vite.org>
2018-12-11 16:06:19 +0800

    cread pod

---
+++ Example/Pods/Target Support Files/Pods-ViteBusiness_Tests/Pods-ViteBusiness_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ViteBusiness_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ViteBusiness_Tests
+@end
---
+++ Example/Pods/Target Support Files/ViteBusiness/ViteBusiness-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ViteBusiness : NSObject
+@end
+@implementation PodsDummy_ViteBusiness
+@end
