6f27f44aba6070448718dfa3f2ae1f9fa3eb8602
Stone <stone.hsia@gmail.com>
2019-04-17 15:36:29 +0800

    RPC batch support request array

--- ViteBusiness/Classes/Components/Vite/AutoGatheringService.swift
+++ ViteBusiness/Classes/Components/Vite/AutoGatheringService.swift
@@ -12,56 +12,128 @@ import PromiseKit
 import RxSwift
 import RxCocoa
 import Vite_HDWalletKit
+import JSONRPCKit
+
+import enum ViteWallet.Result
 
 final class AutoGatheringService {
     static let instance = AutoGatheringService()
     private init() {}
 
     fileprivate let disposeBag = DisposeBag()
-    fileprivate var service: ReceiveTransactionService?
+    fileprivate var service: ReceiveAllTransactionService?
 
     fileprivate var services: [ReceiveTransactionService] = []
 
     func start() {
-
-        Driver.combineLatest(HDWalletManager.instance.walletDriver.filterNil().map({ $0.addressIndex }).distinctUntilChanged(),
-                             HDWalletManager.instance.accountsDriver).drive(onNext: { [weak self] (currectIndex ,accounts) in
-            guard let `self` = self else { return }
-
-            self.services.forEach {
-                $0.stopPoll()
-            }
-            self.services = []
-
+        HDWalletManager.instance.accountsDriver.drive(onNext: { (accounts) in
             if accounts.isEmpty {
+                self.service?.stopPoll()
+                self.service = nil
                 plog(level: .debug, log: "stop receive", tag: .transaction)
             } else {
-                for (index, account) in accounts.enumerated() {
-                    let interval = (index == currectIndex || index == 0) ? 5 : 10
-                    plog(level: .debug, log: account.address.description + ": " + "start receive interval \(interval)", tag: .transaction)
-                    let service = ReceiveTransactionService(account: account, interval: TimeInterval(interval)) { r in
-                        switch r {
-                        case .success(let tuple):
-                            if let (receive, send) = tuple {
-                                if let data = receive.data {
-                                    let bytes = Bytes(data)
-                                    if bytes.count >= 2 && Bytes(bytes[0...1]) == Bytes(arrayLiteral: 0x80, 0x01) {
-                                        GrinManager.default.handle(viteData: Data(bytes.dropFirst(2)), fromAddress: receive.fromAddress?.description ?? "", account: account)
-                                    }
+                plog(level: .debug, log: "start receive for \(accounts.count) address", tag: .transaction)
+                let service = ReceiveAllTransactionService(accounts: accounts, interval: 5, completion: { (r) in
+                    switch r {
+                    case .success(let ret):
+                        for (send, _, account) in ret {
+                            if let data = send.data {
+                                let bytes = Bytes(data)
+                                if bytes.count >= 2 && Bytes(bytes[0...1]) == Bytes(arrayLiteral: 0x80, 0x01) {
+                                    let viteData = Data(bytes.dropFirst(2))
+                                    GrinManager.default.handle(viteData: viteData, fromAddress: send.accountAddress?.description ?? "", account: account)
+                                    let text = String(bytes: viteData, encoding: .utf8) ?? "parse failure"
+                                    plog(level: .debug, log: "found grin data: \(text)", tag: .transaction)
                                 }
-
-                                plog(level: .debug, log: account.address.description + ": " + "receive block hash: \(receive.hash!)", tag: .transaction)
-                            } else {
-                                plog(level: .debug, log: account.address.description + ": " + "no need to receive", tag: .transaction)
                             }
-                        case .failure(let error):
-                            plog(level: .warning, log: account.address.description + ": " + error.viteErrorMessage, tag: .transaction)
                         }
+                        plog(level: .debug, log: "success for receive \(ret.count) blocks", tag: .transaction)
+                    case .failure(let error):
+                        plog(level: .warning, log: "getOnroad for \(accounts.count) address error: \(error.viteErrorMessage)", tag: .transaction)
                     }
-                    service.startPoll()
-                    self.services.append(service)
-                }
+                })
+                service.startPoll()
+                self.service = service
             }
         }).disposed(by: disposeBag)
+    }
+}
+
+extension AutoGatheringService {
+
+    class ReceiveAllTransactionService: PollService {
+        typealias Ret = Result<[(AccountBlock, AccountBlock, Wallet.Account)]>
+
+        public let accounts: [Wallet.Account]
+        public init(accounts: [Wallet.Account], interval: TimeInterval, completion: ((Ret) -> ())? = nil) {
+            self.accounts = accounts
+            self.interval = interval
+            self.completion = completion
+        }
+
+        public var taskId: String = ""
+        public var isPolling: Bool = false
+        public var interval: TimeInterval = 0
+        public var completion: ((Ret) -> ())?
+
+        public func handle(completion: @escaping (Ret) -> ()) {
+            let accounts = self.accounts
+
+            type(of: self).getFirstOnroadIfHas(for: accounts)
+                .map({ accountBlocks -> [(AccountBlock, Wallet.Account)] in
+                    var ret: [(AccountBlock, Wallet.Account)] = []
+                    let array = accountBlocks.compactMap { $0 }
+                    for accountBlock in array {
+                        for account in accounts where accountBlock.toAddress?.description == account.address.description {
+                            ret.append((accountBlock, account))
+                        }
+                    }
+                    return ret
+                }).then({ pairs -> Promise<[(AccountBlock, AccountBlock, Wallet.Account)]> in
+                    let promises = pairs.map { ret -> Promise<(AccountBlock, AccountBlock, Wallet.Account)?> in
+                        return type(of: self).receive(onroadBlock: ret.0, account: ret.1)
+                            .map({ (ret) -> (AccountBlock, AccountBlock, Wallet.Account)? in
+                                return (ret.0, ret.1, ret.2)
+                            })
+                            // ignore error, and return nil
+                            .recover({ (error) -> Promise<(AccountBlock, AccountBlock, Wallet.Account)?> in
+                                plog(level: .warning, log: ret.1.address.description + " receive error: " + error.viteErrorMessage, tag: .transaction)
+                                return .value(nil)
+                            })
+                    }
+                    return when(fulfilled: promises)
+                        // filter nil，make sure when promise success
+                        .map({ (ret) -> [(AccountBlock, AccountBlock, Wallet.Account)] in
+                            return ret.compactMap { $0 }
+                        })
+                }).done({ (ret) in
+                    completion(Result(value: ret))
+                }).catch({ (error) in
+                    completion(Result(error: error))
+                })
+        }
+
+        static func receive(onroadBlock: AccountBlock, account: Wallet.Account) -> Promise<(AccountBlock, AccountBlock, Wallet.Account)> {
+            return Provider.default.receiveTransactionWithoutPow(account: account, onroadBlock: onroadBlock)
+                .recover({ (e) -> Promise<AccountBlock> in
+                    if ViteError.conversion(from: e).code == ViteErrorCode.rpcNotEnoughQuota {
+                        return Provider.default.getPowForReceiveTransaction(account: account, onroadBlock: onroadBlock)
+                        .then({ context -> Promise<AccountBlock> in
+                            return Provider.default.sendRawTxWithContext(context)
+                        })
+                    } else {
+                        return Promise(error: e)
+                    }
+                })
+                .map({ (onroadBlock, $0, account) })
+        }
+
+        static func getFirstOnroadIfHas(for accounts: [Wallet.Account]) -> Promise<[AccountBlock?]> {
+            let requests = accounts.map { GetOnroadBlocksRequest(address: $0.address.description, index: 0, count: 1) }
+            return RPCRequest(for: Provider.default.server, batch: BatchFactory().create(requests)).promise
+                .map { accountBlocksArray -> [AccountBlock?] in
+                    return accountBlocksArray.map { $0.first }
+            }
+        }
     }
 }
---
+++ ViteBusiness/Classes/Components/Vite/BatchFactory+Array.swift
@@ -0,0 +1,133 @@
+//
+//  BatchFactory+Array.swift
+//  ViteBusiness
+//
+//  Created by Stone on 2019/4/16.
+//
+
+import JSONRPCKit
+import Result
+
+private var ke_semaphore: UInt8 = 0
+
+public extension BatchFactory {
+
+    private var e_semaphore: DispatchSemaphore {
+        if let semaphore = objc_getAssociatedObject(self, &ke_semaphore) as? DispatchSemaphore {
+            return semaphore
+        } else {
+            let semaphore = DispatchSemaphore(value: 1)
+            objc_setAssociatedObject(self, &ke_semaphore, semaphore, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            return semaphore
+        }
+    }
+
+    public func create<Request: JSONRPCKit.Request>(_ requests: [Request]) -> BatchArray<Request> {
+        _ = e_semaphore.wait(timeout: DispatchTime.distantFuture)
+        let batchElements = requests.map { BatchElement(request: $0, version: version, id: idGenerator.next())}
+        e_semaphore.signal()
+
+        return BatchArray(batchElements: batchElements)
+    }
+}
+
+public struct BatchArray<Request: JSONRPCKit.Request>: Batch {
+    public typealias Responses = [Request.Response]
+    public typealias Results = [Result<Request.Response, JSONRPCError>]
+
+    public let batchElements: [BatchElement<Request>]
+
+    public var requestObject: Any {
+        return batchElements.map { $0.body }
+    }
+
+    public func responses(from object: Any) throws -> Responses {
+        guard let batchObjects = object as? [Any] else {
+            throw JSONRPCError.nonArrayResponse(object)
+        }
+
+        return try batchElements.map { try $0.e_response(from: batchObjects) }
+    }
+
+    public func results(from object: Any) -> Results {
+        guard let batchObjects = object as? [Any] else {
+            return batchElements.map { _ in .failure(.nonArrayResponse(object)) }
+        }
+
+        return batchElements.map { $0.e_result(from: batchObjects) }
+    }
+
+    public static func responses(from results: Results) throws -> Responses {
+        return try results.map { try $0.dematerialize()}
+    }
+}
+
+fileprivate extension BatchElement {
+    // Copy from https://github.com/bricklife/JSONRPCKit/blob/3.0.0/Sources/JSONRPCKit/BatchElement.swift
+    internal func e_response(from object: Any) throws -> Request.Response {
+        switch e_result(from: object) {
+        case .success(let response):
+            return response
+
+        case .failure(let error):
+            throw error
+        }
+    }
+
+    /// - Throws: JSONRPCError
+    internal func e_response(from objects: [Any]) throws -> Request.Response {
+        switch e_result(from: objects) {
+        case .success(let response):
+            return response
+
+        case .failure(let error):
+            throw error
+        }
+    }
+
+    internal func e_result(from object: Any) -> Result<Request.Response, JSONRPCError> {
+        guard let dictionary = object as? [String: Any] else {
+            return .failure(.unexpectedTypeObject(object))
+        }
+
+        let receivedVersion = dictionary["jsonrpc"] as? String
+        guard version == receivedVersion else {
+            return .failure(.unsupportedVersion(receivedVersion))
+        }
+
+        guard id == dictionary["id"].flatMap(Id.init) else {
+            return .failure(.responseNotFound(requestId: id, object: dictionary))
+        }
+
+        let resultObject = dictionary["result"]
+        let errorObject = dictionary["error"]
+
+        switch (resultObject, errorObject) {
+        case (nil, let errorObject?):
+            return .failure(JSONRPCError(errorObject: errorObject))
+
+        case (let resultObject?, nil):
+            do {
+                return .success(try request.response(from: resultObject))
+            } catch {
+                return .failure(.resultObjectParseError(error))
+            }
+
+        default:
+            return .failure(.missingBothResultAndError(dictionary))
+        }
+    }
+
+    internal func e_result(from objects: [Any]) -> Result<Request.Response, JSONRPCError> {
+        let matchedObject = objects
+            .flatMap { $0 as? [String: Any] }
+            .filter { $0["id"].flatMap(Id.init) == id }
+            .first
+
+        guard let object = matchedObject else {
+            return .failure(.responseNotFound(requestId: id, object: objects))
+        }
+
+        return e_result(from: object)
+    }
+}
