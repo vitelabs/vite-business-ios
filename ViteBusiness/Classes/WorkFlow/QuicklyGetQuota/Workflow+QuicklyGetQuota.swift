fc804173bdd78eb68ca4c21c15b74a7109af8628
Stone <stone.hsia@gmail.com>
2019-08-16 15:29:38 +0800

    fix quickly get quota amount

--- ViteBusiness/Classes/WorkFlow/QuicklyGetQuota/Workflow+QuicklyGetQuota.swift
+++ ViteBusiness/Classes/WorkFlow/QuicklyGetQuota/Workflow+QuicklyGetQuota.swift
@@ -17,6 +17,14 @@ import enum Alamofire.Result
 public extension Workflow {
 
     public static func quicklyGetQuota(completion: @escaping (Result<AccountBlock>) -> ()) {
+
+        let balance = ViteBalanceInfoManager.instance.balanceInfo(forViteTokenId: ViteWalletConst.viteToken.id)?.balance ?? Amount(0)
+        guard balance >= "134".toAmount(decimals: ViteWalletConst.viteToken.decimals)! else {
+            Toast.show(R.string.localizable.sendPageToastAmountError())
+            completion(Result.failure(ViteError.cancel))
+            return
+        }
+
         QuicklyGetQuotaConfirmView(completion: { (_, ret) in
             completion(ret)
         }, canceled: { (_) in
